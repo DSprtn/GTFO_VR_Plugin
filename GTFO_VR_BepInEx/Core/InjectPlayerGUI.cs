ea567b866a6fc48400efb84e58323dba74a38791
Dsprtn <drtsprtn@gmail.com>
2020-02-04 21:28:07 +0100

    Tweak player HUD

--- GTFO_VR_BepInEx/Core/InjectPlayerGUI.cs
+++ GTFO_VR_BepInEx/Core/InjectPlayerGUI.cs
@@ -10,7 +10,8 @@ using UnityEngine;
 namespace GTFO_VR_BepInEx.Core
 {
     /// <summary>
-    /// Entry point for loading and initiating all things VR
+    /// Hacky thing to get the GUI visible to the player inside the HMD --- 
+    /// Basically moves all UI elements more towards the center to account for the bigger screen size necessary for lens distortion
     /// </summary>
 
     [HarmonyPatch(typeof(PlayerGuiLayer),"Setup")]
@@ -18,17 +19,16 @@ namespace GTFO_VR_BepInEx.Core
     {
         static void Postfix(PlayerGuiLayer __instance, PUI_Inventory ___Inventory, PUI_LocalPlayerStatus ___m_playerStatus, PUI_WardenIntel ___m_wardenIntel, PUI_GameEventLog ___m_gameEventLog, PUI_Compass ___m_compass, PUI_GameObjectives ___m_wardenObjective)
         {
-            ___Inventory.SetPosition(new Vector2(-450f, -200f));
-            ___Inventory.transform.localScale *= .85f;
+            ___Inventory.SetPosition(new Vector2(-475f, -225f));
+            ___Inventory.transform.localScale *= .80f;
             ___m_compass.SetPosition(new Vector2(0.0f, -150f));
             ___m_compass.transform.localScale *= .8f;
             ___m_gameEventLog.SetPosition(new Vector2(150f, 50f));
 
-            ___m_wardenIntel.SetPosition(new Vector2(400f, -100f));
-            ___m_wardenIntel.SetSize(new Vector2(325, 350f));
-            ___m_wardenIntel.transform.localScale *= .85f;
-            ___m_wardenObjective.SetPosition(new Vector2(400f, -300f));
-            ___m_wardenObjective.transform.localScale *= .7f;
+            ___m_wardenIntel.SetPosition(new Vector2(450f, -100f));
+            ___m_wardenIntel.transform.localScale *= .6f;
+            ___m_wardenObjective.SetPosition(new Vector2(450f, -300f));
+            ___m_wardenObjective.transform.localScale *= .65f;
             ___m_playerStatus.SetPosition(new Vector2(0.0f, 80f));
         }
     }
