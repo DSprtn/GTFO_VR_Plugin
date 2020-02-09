3c72d67ef0e7f055aa00c654b4bc2361bb454cb6
Dsprtn <drtsprtn@gmail.com>
2020-02-09 01:58:02 +0100

    Fix laserpointer + dot

--- GTFO_VR/LaserPointer.cs
+++ GTFO_VR/LaserPointer.cs
@@ -6,14 +6,11 @@ namespace GTFO_VR
 {
     public class LaserPointer : MonoBehaviour
     {
-        public bool active = true;
         public float thickness = 1f / 400f;
 
         public Color color = Color.red;
-        public GameObject holder;
         public GameObject pointer;
         public GameObject dot;
-        public int layerMask = 0;
 
         public Vector3 dotScale = new Vector3(0.04f, 0.01f, 0.016f);
 
@@ -27,45 +24,48 @@ namespace GTFO_VR
             CreatePointerObjects();
         }
 
-        private void Update()
+        private void LateUpdate()
         {
-            if (pointer.activeSelf)
+            if(transform.parent == null)
             {
-                float hitDistance = 35f;
-                RaycastHit hitInfo;
-                bool raycastHit = Physics.Raycast(transform.position, transform.forward, out hitInfo, hitDistance, layerMask);
-
-                if (raycastHit && hitInfo.distance < hitDistance)
-                {
-                    hitDistance = hitInfo.distance;
-                    dot.SetActive(true);
-                    dot.transform.position = hitInfo.point;
-                    dot.transform.rotation = Quaternion.LookRotation(pointer.transform.up);
-                }
-                else
-                {
-                    dot.SetActive(false);
-                }
-
-                pointer.transform.localScale = new Vector3(this.thickness, this.thickness, hitDistance);
-                pointer.transform.localPosition = new Vector3(0.0f, 0.0f, hitDistance / 2f);
+                return;
             }
+            float dist = 100f;
+
+            Ray raycast = new Ray(transform.parent.position, transform.parent.forward);
+            RaycastHit hit;
+            bool bHit = Physics.Raycast(raycast, out hit, 101f, LayerManager.MASK_WEAPON_TARGETS, QueryTriggerInteraction.Ignore);
+
+            if (bHit && hit.distance < 100f)
+            {
+                dist = hit.distance;
+                dot.transform.rotation = Quaternion.LookRotation(pointer.transform.up);
+                dot.transform.position = hit.point;
+
+            }
+            else
+            {
+                dot.SetActive(false);
+            }
+
+            pointer.transform.localScale = new Vector3(thickness, thickness, dist);
+            pointer.transform.localPosition = new Vector3(0f, 0f, dist / 2f);
+
         }
 
 
         public void PlayerChangedItem(ItemEquippable item)
         {
-            
+
             if (item.HasFlashlight && item.AmmoType != Player.AmmoType.None)
             {
                 EnablePointer();
+                SetHolderTransform(item.MuzzleAlign);
             }
             else
             {
                 DisablePointer();
             }
-
-            SetHolderTransform(item.MuzzleAlign);
         }
 
         void EnablePointer()
@@ -80,27 +80,30 @@ namespace GTFO_VR
             dot.SetActive(false);
         }
 
-        void SetHolderTransform(Transform t) 
+        void SetHolderTransform(Transform t)
         {
-            holder.transform.SetParent(t);
-            holder.transform.localPosition = Vector3.zero;
-            holder.transform.localRotation = Quaternion.identity;
+            transform.SetParent(t);
+            transform.localPosition = Vector3.zero;
+            transform.localRotation = Quaternion.identity;
+
+            pointer.transform.localScale = new Vector3(thickness, thickness, 100f);
+            pointer.transform.localPosition = new Vector3(0.0f, 0.0f, 50f);
+            dot.transform.localPosition = new Vector3(0.0f, 0.0f, 50f);
+            pointer.transform.localRotation = Quaternion.identity;
+            dot.transform.localRotation = Quaternion.identity;
+            dot.transform.localScale = dotScale;
         }
         private void CreatePointerObjects()
         {
-            holder = new GameObject();
-            holder.transform.parent = transform;
-            holder.transform.localPosition = Vector3.zero;
-            holder.transform.localRotation = Quaternion.identity;
             pointer = GameObject.CreatePrimitive(PrimitiveType.Cube);
             dot = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
             dot.transform.localScale = dotScale;
-            dot.transform.SetParent(holder.transform);
+            dot.transform.SetParent(transform);
             dot.GetComponent<Collider>().enabled = false;
             pointer.GetComponent<Collider>().enabled = false;
 
-            pointer.transform.parent = this.holder.transform;
-            pointer.transform.localScale = new Vector3(this.thickness, this.thickness, 100f);
+            pointer.transform.parent = transform;
+            pointer.transform.localScale = new Vector3(thickness, thickness, 100f);
             pointer.transform.localPosition = new Vector3(0.0f, 0.0f, 50f);
             dot.transform.localPosition = new Vector3(0.0f, 0.0f, 50f);
             pointer.transform.localRotation = Quaternion.identity;
