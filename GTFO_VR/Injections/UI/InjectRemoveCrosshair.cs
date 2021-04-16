212417e1421bfe10b6db59330357116d87f755e4
Dsprtn <drtsprtn@gmail.com>
2021-04-17 00:28:28 +0200

    Bring back crosshair removal injection. Oops.

---
+++ GTFO_VR/Injections/UI/InjectRemoveCrosshair.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using GTFO_VR;
+using HarmonyLib;
+using Player;
+using UnityEngine;
+
+
+namespace GTFO_VR_BepInEx.Core
+{
+    /// <summary>
+    /// Disable crosshair GUI layer, if this is not done the crosshair would be visible in the 2D game view on screen
+    /// </summary>
+    [HarmonyPatch(typeof(GuiManager), nameof(GuiManager.OnFocusStateChanged))]
+    class InjectRemoveCrosshairHit
+    {
+        static void Postfix()
+        {
+            GuiManager.CrosshairLayer.SetVisible(false);
+        }
+    }
+}
