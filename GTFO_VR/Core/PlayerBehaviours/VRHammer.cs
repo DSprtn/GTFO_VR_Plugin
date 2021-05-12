240b904c136d577c8a0c66e53db30a81457708e5
Dsprtn <drtsprtn@gmail.com>
2021-05-13 01:08:28 +0200

    Tweak hammer hitbox, add auto-release hit on hammer, add visual charge indicator

--- GTFO_VR/Core/PlayerBehaviours/VRHammer.cs
+++ GTFO_VR/Core/PlayerBehaviours/VRHammer.cs
@@ -2,16 +2,12 @@
 using GTFO_VR.Core.VR_Input;
 using GTFO_VR.Events;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
 
 namespace GTFO_VR.Core.PlayerBehaviours
 {
     /// <summary>
-    /// For now the only purpose of this class is to disable the melee weapon's animations
+    /// Disable animations, tweak hitbox size and position
     /// </summary>
     public class VRHammer : MonoBehaviour
     {
@@ -19,24 +15,85 @@ namespace GTFO_VR.Core.PlayerBehaviours
         {
         }
 
-        public static float hammerSizeMult = .9f;
+        public static float hammerSizeMult = .63f;
 
-        MeleeWeaponFirstPerson m_weapon;
-        Transform m_animatorRoot;
+        private MeleeWeaponFirstPerson m_weapon;
+        private Transform m_animatorRoot;
+        private Light m_chargeupIndicatorLight;
 
         public void Setup(MeleeWeaponFirstPerson weapon)
         {
             m_weapon = weapon;
             m_animatorRoot = m_weapon.ModelData.m_damageRefAttack.parent;
+            m_chargeupIndicatorLight = new GameObject("VR_Hammer_Chargeup_Light").AddComponent<Light>();
+            m_chargeupIndicatorLight.color = Color.white;
+            m_chargeupIndicatorLight.range = 0.2f;
+            m_chargeupIndicatorLight.enabled = false;
+            m_chargeupIndicatorLight.shadows = LightShadows.None;
+            HammerEvents.OnHammerFullyCharged += HammerFullyCharged;
         }
 
-        void LateUpdate()
+        private void HammerFullyCharged()
         {
-            if(VRConfig.configUseOldHammer.Value || !VRConfig.configUseControllers.Value)
+            if (!VRConfig.configUseVisualHammerIndicator.Value)
             {
                 return;
             }
-            if(m_weapon.Owner && m_weapon.Owner.IsLocallyOwned)
+
+            if (m_chargeupIndicatorLight != null)
+            {
+                m_chargeupIndicatorLight.enabled = true;
+            }
+            Invoke(nameof(VRHammer.TurnChargeLightOff), 0.1f);
+        }
+
+        private void TurnChargeLightOff()
+        {
+            m_chargeupIndicatorLight.enabled = false;
+        }
+
+        private void Update()
+        {
+            if (VRConfig.configUseOldHammer.Value || !VRConfig.configUseControllers.Value)
+            {
+                return;
+            }
+            if (m_weapon.Owner && m_weapon.Owner.IsLocallyOwned)
+            {
+                ForceDamageRefPosition();
+                m_chargeupIndicatorLight.transform.position = m_weapon.ModelData.m_damageRefAttack.transform.position;
+            }
+        }
+
+        private void ForceDamageRefPosition()
+        {
+            if (VRConfig.configUseControllers.Value && !VRConfig.configUseOldHammer.Value)
+            {
+                if (m_weapon.ModelData != null)
+                {
+                    float YOffset = .68f;
+                    float zOffset = .45f;
+                    m_weapon.ModelData.m_damageRefAttack.transform.position = Controllers.mainController.transform.TransformPoint(new Vector3(0, YOffset, zOffset));
+                }
+            }
+        }
+
+        private void LateUpdate()
+        {
+            if (VRConfig.configUseOldHammer.Value || !VRConfig.configUseControllers.Value)
+            {
+                return;
+            }
+
+            if (GTFO_VR_Plugin.DEBUG_ENABLED)
+            {
+                m_weapon._DEBUG_TARGETING_ENABLED_k__BackingField = VRConfig.configDebugShowHammerHitbox.Value;
+                m_weapon._DEBUG_TARGETING_ENABLED_k__BackingField = VRConfig.configDebugShowHammerHitbox.Value;
+
+                hammerSizeMult = VRConfig.configDebugHammersizeMult.Value;
+            }
+
+            if (m_weapon.Owner && m_weapon.Owner.IsLocallyOwned)
             {
                 if (FocusStateEvents.currentState == eFocusState.FPS)
                 {
@@ -46,5 +103,9 @@ namespace GTFO_VR.Core.PlayerBehaviours
             }
         }
 
+        private void OnDestroy()
+        {
+            HammerEvents.OnHammerFullyCharged -= HammerFullyCharged;
+        }
     }
 }
--- GTFO_VR/Injections/Gameplay/InjectVRHammer.cs
+++ GTFO_VR/Injections/Gameplay/InjectVRHammer.cs
@@ -4,10 +4,12 @@ using GTFO_VR.Core;
 using GTFO_VR.Core.PlayerBehaviours;
 using GTFO_VR.Core.VR_Input;
 using GTFO_VR.Events;
+using GTFO_VR.Util;
 using HarmonyLib;
 using Il2CppSystem.Collections.Generic;
 using LevelGeneration;
 using UnityEngine;
+
 
 namespace GTFO_VR.Injections
 {
@@ -17,9 +19,77 @@ namespace GTFO_VR.Injections
     [HarmonyPatch(typeof(MeleeWeaponFirstPerson), nameof(MeleeWeaponFirstPerson.Setup))]
     static class InjectVRHammer
     {
+
         static void Postfix(MeleeWeaponFirstPerson __instance)
         {
             __instance.gameObject.AddComponent<VRHammer>().Setup(__instance);
+        }
+    }
+
+    /// <summary>
+    /// Auto-Release charge when smackin something
+    /// </summary>
+    [HarmonyPatch(typeof(MWS_ChargeUp), nameof(MWS_ChargeUp.Update))]
+    static class InjectAutoReleaseHammerSmack
+    {
+
+        static void Postfix(MWS_ChargeUp __instance)
+        {
+
+            if (Controllers.mainControllerPose.GetVelocity().magnitude > 0.4f)
+            {
+                Collider[] staticColliders = Physics.OverlapSphere(__instance.m_weapon.ModelData.m_damageRefAttack.position, VRHammer.hammerSizeMult * .25f, LayerManager.MASK_MELEE_ATTACK_TARGETS_WITH_STATIC);
+                Collider[] enemyColliders = Physics.OverlapSphere(__instance.m_weapon.ModelData.m_damageRefAttack.position, VRHammer.hammerSizeMult * .75f, LayerManager.MASK_ENEMY_DAMAGABLE);
+                if (GTFO_VR_Plugin.DEBUG_ENABLED)
+                {
+                    if(VRConfig.configDebugShowHammerHitbox.Value)
+                    {
+                        DebugDraw3D.DrawSphere(__instance.m_weapon.ModelData.m_damageRefAttack.position, VRHammer.hammerSizeMult, ColorExt.Blue(0.2f));
+                        DebugDraw3D.DrawSphere(__instance.m_weapon.ModelData.m_damageRefAttack.position, VRHammer.hammerSizeMult, ColorExt.Red(0.2f));
+                    }
+                }
+                if (staticColliders.Length > 0 || enemyColliders.Length > 0)
+                {
+                    __instance.OnChargeupRelease();
+                }
+            }
+        }
+    }
+
+
+    /// <summary>
+    /// Sort hits to prioritize them based on proximity to hammer center (only first enemy hit counts)
+    /// </summary>
+    [HarmonyPatch(typeof(MWS_AttackSwingBase), nameof(MWS_AttackSwingBase.OnAttackHit))]
+    static class InjectPrioritizeProximityTargetsOnHit
+    {
+
+        static void Postfix(MWS_AttackSwingBase __instance)
+        {
+            var hits = __instance.m_weapon.HitsForDamage;
+
+            List<MeleeWeaponDamageData> sortedHits = new List<MeleeWeaponDamageData>();
+            
+            Vector3 damageRefPos = __instance.m_data.m_damageRef.position;
+
+            
+            while(hits.Count > 0)
+            {
+                float lowest = 999999f;
+                MeleeWeaponDamageData closestData = null;
+                foreach(var hit in hits)
+                {
+                    float sqrDst = (hit.hitPos - damageRefPos).sqrMagnitude;
+                    if (sqrDst <= lowest)
+                    {
+                        closestData = hit;
+                        lowest = sqrDst;
+                    }
+                }
+                sortedHits.Add(closestData);
+                hits.Remove(closestData);
+            }
+            __instance.m_weapon.HitsForDamage = sortedHits;
         }
     }
 
@@ -44,6 +114,30 @@ namespace GTFO_VR.Injections
             else
             {
                 HammerEvents.HammerSmacked(__instance.m_damageToDeal / __instance.m_damageHeavy);
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(MWS_ChargeUp), nameof(MWS_ChargeUp.Enter))]
+    internal class InjectHammerChargeEnter
+    {
+        private static void Prefix(MWS_ChargeUp __instance)
+        {
+            InjectHammerChargeEvents.hammerChargeEventFired = false;
+        }
+    }
+
+    [HarmonyPatch(typeof(MWS_ChargeUp), nameof(MWS_ChargeUp.Update))]
+    internal class InjectHammerChargeEvents
+    {
+        internal static bool hammerChargeEventFired;
+        private static void Prefix(MWS_ChargeUp __instance)
+        {
+            float progress = Mathf.Min(__instance.m_elapsed / __instance.m_maxDamageTime, 1f);
+            if (progress >= 1 && !hammerChargeEventFired)
+            {
+                HammerEvents.HammerFullyCharged();
+                hammerChargeEventFired = true;
             }
         }
     }
