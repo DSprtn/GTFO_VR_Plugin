f17396d45a1c3a564d37e32b3f826d39738261b1
Roughy <mdawn700@gmail.com>
2022-05-20 21:46:05 +0500

    Reimplement OnRectTransformDimensionsChange() so we don't have to update BepInEx

--- GTFO_VR/Core/UI/canvas/KeyboardDefinition/RectColliderSizer.cs
+++ GTFO_VR/Core/UI/canvas/KeyboardDefinition/RectColliderSizer.cs
@@ -9,9 +9,14 @@ using UnityEngine.EventSystems;
 
 namespace GTFO_VR.Core.UI.canvas.KeyboardDefinition
 {
-    class RectColliderSizer : UIBehaviour
+    class RectColliderSizer : MonoBehaviour // Replacement for UIBehaviour::OnRectTransformDimensionsChange() because unhollowed libs don't have the virtual modifier (fixed by updating bepinex )
     {
-        public override void OnRectTransformDimensionsChange()
+        RectTransform m_rectTransform;
+
+        private float m_Width;
+        private float m_Height;
+
+        void handleResize()
         {
             BoxCollider collider = GetComponent<BoxCollider>();
             RectTransform trans = GetComponent<RectTransform>();
@@ -20,6 +25,30 @@ namespace GTFO_VR.Core.UI.canvas.KeyboardDefinition
             collider.size = new Vector3(trans.sizeDelta.x * TerminalKeyboardInterface.HITBOX_SCALE, trans.sizeDelta.y * TerminalKeyboardInterface.HITBOX_SCALE, collider.size.z);
         }
 
+        void Start()
+        {
+            m_rectTransform = GetComponent<RectTransform>();
+            if (m_rectTransform != null)
+            {
+                m_Width = m_rectTransform.sizeDelta.x;
+                m_Height = m_rectTransform.sizeDelta.y;
+            }
 
+            handleResize();
+        }
+
+        void Update()
+        {
+            if (m_rectTransform != null)
+            {
+                if (m_rectTransform.sizeDelta.x != m_Width || m_rectTransform.sizeDelta.y != m_Height)
+                {
+                    m_Width = m_rectTransform.sizeDelta.x;
+                    m_Height = m_rectTransform.sizeDelta.y;
+
+                    handleResize();
+                }
+            }
+        }
     }
 }
