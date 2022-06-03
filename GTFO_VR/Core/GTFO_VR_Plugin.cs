e8db7594415474409888dd32e5d2f231291f9a98
Dsprtn <drtsprtn@gmail.com>
2022-06-03 19:35:30 +0200

    Bump version, add packaged builds to gitignore for convenience

--- GTFO_VR/Core/GTFO_VR_Plugin.cs
+++ GTFO_VR/Core/GTFO_VR_Plugin.cs
@@ -24,7 +24,7 @@ namespace GTFO_VR.Core
             MODNAME = "GTFO_VR_Plugin",
             AUTHOR = "Spartan",
             GUID = "com." + AUTHOR + "." + MODNAME,
-            VERSION = "1.1.9.5";
+            VERSION = "1.1.9.5b";
 
 
         public static bool DEBUG_ENABLED = false;
