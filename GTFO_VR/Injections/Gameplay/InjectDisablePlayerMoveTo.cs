37f60cbfdc30fe55ea6d65db6572688c8f1c58d4
Roughy <mdawn700@gmail.com>
2022-06-17 20:29:25 +0500

    Fixed player moving forward and to the sides when in terminal

---
+++ GTFO_VR/Injections/Gameplay/InjectDisablePlayerMoveTo.cs
@@ -0,0 +1,23 @@
+﻿using HarmonyLib;
+using Player;
+
+namespace GTFO_VR.Injections.Gameplay
+{
+    /// <summary>
+    /// Game will attempt to move player closer to terminal, but uses camera forward and sends you sliding off to the side. Disable when in terminal.
+    /// </summary>
+    ///
+    [HarmonyPatch(typeof(PlayerCharacterController), nameof(PlayerCharacterController.MoveTo))]
+    internal class InjectExternalForce
+    {
+        private static bool Prefix(PlayerCharacterController __instance)
+        {
+
+            if (__instance.m_owner.IsLocallyOwned && FocusStateManager.CurrentState == eFocusState.ComputerTerminal)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
