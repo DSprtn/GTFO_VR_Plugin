49ec993e34496732ca73e5b01c908b47a9ebb47b
Mathias Nordskog <mdawn700@gmail.com>
2022-07-04 03:22:23 +0500

    Fixed watch objectives, include progressions (#36)

--- GTFO_VR/Injections/UI/InjectWatchObjectives.cs
+++ GTFO_VR/Injections/UI/InjectWatchObjectives.cs
@@ -3,33 +3,56 @@ using GTFO_VR.Core;
 using GTFO_VR.UI;
 using HarmonyLib;
 using LevelGeneration;
+using Localization;
 
 namespace GTFO_VR.Injections.UI
 {
-    // As of R7 this patch will crash the game when it is called, at random. Commented out for now.
-
     /// <summary>
-    /// Replicate new objectives on the VR watch
+    /// Replicate new objectives on the VR watch. 
     /// </summary>
-    /*
+    
     [HarmonyPatch(typeof(PlayerGuiLayer), nameof(PlayerGuiLayer.UpdateObjectives))]
     internal class InjectWatchObjectives
     {
-        private static void Postfix(LG_LayerType layer,string mainObjective)
+        public static PUI_GameObjectives PlayerGuiLayer_PUI_GameObjectives_Instance = null;
+
+        private static void Prefix(PlayerGuiLayer __instance)
         {
-            Watch.Current?.UpdateMainObjective(mainObjective);
-            Log.Debug($"Got new objective! - {mainObjective}");
+            // Keep track of the instance so we can skip the PUI_GameObjectives.SetProgressionObjective() patch
+            // when called on any other instance. Unfortunately patching that method is the only way.
+            PlayerGuiLayer_PUI_GameObjectives_Instance = __instance.WardenObjectives;
+        }
+
+        private static void Postfix(PlayerGuiLayer __instance)
+        {
+            Watch.Current?.UpdateObjective(__instance.m_wardenObjective);
+            Log.Debug($"Got new objectives!");
+        }
+    }
+    
+    [HarmonyPatch(typeof(PUI_GameObjectives), nameof(PUI_GameObjectives.SetProgressionObjective))]
+    internal class InjectWatchObjectivesProgression
+    {
+        private static void Postfix(PUI_GameObjectives __instance)
+        {
+            if ( __instance == InjectWatchObjectives.PlayerGuiLayer_PUI_GameObjectives_Instance)
+            {
+                Watch.Current?.UpdateObjective(__instance);
+                Log.Debug($"Got new progression!");
+            }
         }
     }
 
-    [HarmonyPatch(typeof(PUI_GameObjectives), nameof(PUI_GameObjectives.SetMainSubObjective))]
-    internal class InjectWatchSubObjectives
+    [HarmonyPatch(typeof(PUI_GameObjectives), nameof(PUI_GameObjectives.RemoveProgressionObjective))]
+    internal class InjectWatchObjectivesProgressionRemove
     {
-        private static void Postfix(string txt)
+        private static void Postfix(PUI_GameObjectives __instance)
         {
-            Watch.Current?.UpdateSubObjective(txt);
-            Log.Debug($"Got new subobjective! - {txt}");
+            if (__instance == InjectWatchObjectives.PlayerGuiLayer_PUI_GameObjectives_Instance)
+            {
+                Watch.Current?.UpdateObjective(__instance);
+                Log.Debug($"Got progression removal!");
+            }
         }
     }
-    */
 }
