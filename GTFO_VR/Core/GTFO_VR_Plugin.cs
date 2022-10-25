189a731bfdf2467c4ea78e3bbc41041331553e58
Dsprtn <drtsprtn@gmail.com>
2022-10-25 21:03:27 +0200

    Update changelog, bump version

--- GTFO_VR/Core/GTFO_VR_Plugin.cs
+++ GTFO_VR/Core/GTFO_VR_Plugin.cs
@@ -24,7 +24,7 @@ namespace GTFO_VR.Core
             MODNAME = "GTFO_VR_Plugin",
             AUTHOR = "Spartan",
             GUID = "com." + AUTHOR + "." + MODNAME,
-            VERSION = "1.1.9.12";
+            VERSION = "1.1.9.13";
 
 
         public static bool DEBUG_ENABLED = false;
