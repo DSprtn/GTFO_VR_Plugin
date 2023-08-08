c2011d37c7aab4d2d53d2cad77bb603af59693d7
Roughy <mdawn700@gmail.com>
2023-08-08 23:41:20 +0500

    Consider piercing, temporal order of hits

--- GTFO_VR/Core/PlayerBehaviours/VRMeleeWeapon.cs
+++ GTFO_VR/Core/PlayerBehaviours/VRMeleeWeapon.cs
@@ -57,9 +57,9 @@ namespace GTFO_VR.Core.PlayerBehaviours
             {
                 case "Spear":
                     m_hitboxSize = 0.035f;
-                    m_offsetTip = new Vector3(0, 1f, 0f);
-                    m_offsetBase = new Vector3(0, 0.8f, 0f);
-                    m_elongatedHitbox = true;
+                    m_offsetTip = new Vector3(0, 0.95f, 0f);
+                    m_offsetBase = m_offsetTip;
+                    m_elongatedHitbox = false;
                     m_centerHitbox = false;
                     break;
                 case "Knife":
@@ -254,10 +254,23 @@ namespace GTFO_VR.Core.PlayerBehaviours
             }
         }
 
-        private record MeleeAttackData(Vector3 weaponPosCurrent, Vector3 weaponPosPrev);
+        private record MeleeAttackData(Vector3 weaponPosCurrent, Vector3 weaponPosPrev)
+        {
+            public Vector3 Velocity
+            {
+                get { return (weaponPosCurrent - weaponPosPrev); }
+            }
+        }
+
+        private record MeleeAttackHit(RaycastHit rayHit, Vector3 weaponPosCurrent, Vector3 weaponPosPrev, float hitTime)
+        {
+            public Vector3 Velocity
+            {
+                get { return (weaponPosCurrent - weaponPosPrev); }
+            }
+        }
 
 
-        
         public bool CheckForAttackTarget( out List<MeleeWeaponDamageData> hits)
         {
             HandleSkinnedDoor();
@@ -290,57 +303,105 @@ namespace GTFO_VR.Core.PlayerBehaviours
                 }
             }
 
-            bool castHit = false;
+            float earliestHitTime = float.MaxValue; // Temporal distance of nearest hit
+            float earliestObstacleHitTime = float.MaxValue; // Temporal distance of nearest obstacle hit
+
+            // May be populated by a single hit ( earliest SphereCast hit ) or a bunch of hits from SphereCastAll ( spear ) 
+            System.Collections.Generic.List<MeleeAttackHit> attackHits = new System.Collections.Generic.List<MeleeAttackHit>();
             foreach (var hitbox in hitboxes)
             {
-                var ( weaponPosCurrent, weaponPosPrev )= (hitbox.weaponPosCurrent, hitbox.weaponPosPrev);
-                Vector3 velocity = (weaponPosCurrent - weaponPosPrev);
 
 #if DEBUG_GTFO_VR
-                DebugDraw3D.DrawCone(weaponPosCurrent, weaponPosPrev, m_hitboxSize * 0.3f, ColorExt.Blue(0.5f), 0.5f);
+                DebugDraw3D.DrawCone(hitbox.weaponPosCurrent, hitbox.weaponPosPrev, m_hitboxSize * 0.3f, ColorExt.Blue(0.5f), 0.5f);
 #endif
-
                 // cast a sphere from where the the hitbox was, to where it is, and get the first thing it collides with along the way
-                RaycastHit rayHit;
-                castHit = Physics.SphereCast(weaponPosPrev, m_hitboxSize, velocity.normalized, out rayHit, velocity.magnitude, LayerManager.MASK_MELEE_ATTACK_TARGETS_WITH_STATIC, QueryTriggerInteraction.Ignore);
-                if (castHit)
+                if (m_weapon.MeleeArchetypeData.CanHitMultipleEnemies)
                 {
-                    IDamageable damagable = rayHit.collider.GetComponent<IDamageable>();
+                    // Spear  is able to hit multiple enemies 
+                    RaycastHit[] rayHits = Physics.SphereCastAll(hitbox.weaponPosPrev, m_hitboxSize, hitbox.Velocity.normalized, hitbox.Velocity.magnitude, LayerManager.MASK_MELEE_ATTACK_TARGETS_WITH_STATIC, QueryTriggerInteraction.Ignore);
 
-                    // Check if we've already hit this collider ( this is the first check, so no )
-                    // and also sets identifier so we can make sure we don't double-tap it below
-                    if (ConsiderDamageable(damagable))
+                    // Order is not guaranteed, so sort them nearest-to-furthest.
+                    // This matters because each enemy can only be hit once
+                    if (rayHits.Length > 1)
                     {
-#if DEBUG_GTFO_VR
-                        DebugDraw3D.DrawCone(weaponPosCurrent, weaponPosPrev, m_hitboxSize * 0.3f, ColorExt.Blue(0.5f), DEBUG_HIT_DRAW_DURATION);
-#endif
-                        hits.Add(new MeleeWeaponDamageData
-                        {
-                            damageGO = rayHit.collider.gameObject,
-                            hitPos = rayHit.point, // vector from sourcePos to hitPos, and source to enemy spine used to determine backstab
-                            hitNormal = rayHit.normal, // Used for gore
-                            sourcePos = rayHit.point - (velocity.normalized), // positioned used to calculate backstab
-                            damageTargetFound = true, // Not actually used for anything
-                            damageComp = damagable
-                        });
+                        System.Array.Sort(rayHits, (a, b) => a.distance.CompareTo(b.distance));
                     }
 
-                    // Unless we are looking for multiple hits, break on the first hit
-                    if (!m_weapon.MeleeArchetypeData.CanHitMultipleEnemies)
-                        break;
+                    // Remove any hits that came after hitting an obstacle, so we can't stab through obstacles.
+                    foreach (var rayHit in rayHits)
+                    {
+                        float hitTime = rayHit.distance / hitbox.Velocity.magnitude;
+                        attackHits.Add(new MeleeAttackHit(rayHit, hitbox.weaponPosCurrent, hitbox.weaponPosPrev, hitTime));
+
+                        // If there are multiple hitboxes we have to check them all before filtering ones that
+                        // collided after hitting an bstacle. We do this below.
+                        if (rayHit.collider.GetComponent<IDamageable>() == null)
+                        {
+                            if (hitTime < earliestObstacleHitTime)
+                                earliestObstacleHitTime = hitTime;
+
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    // Other weapons only care about the first hit
+                    RaycastHit rayHit;
+                    if (Physics.SphereCast(hitbox.weaponPosPrev, m_hitboxSize, hitbox.Velocity.normalized, out rayHit, hitbox.Velocity.magnitude, LayerManager.MASK_MELEE_ATTACK_TARGETS_WITH_STATIC, QueryTriggerInteraction.Ignore))
+                    {
+                        // Hitboxes may impact different colliders on the same frame
+                        // % it travelled along the ray should correspond roughly to what time the impact was made,
+                        // so we can figure out whether we hit something before or after the other hitboxes.
+                        float hitTime = rayHit.distance / hitbox.Velocity.magnitude;
+                        if (hitTime < earliestHitTime)
+                        {
+                            // While we check multiple hitboxes, we only ever care for the earliest hit
+                            // so we can just replace the existing hit here.
+                            attackHits.Clear();
+                            attackHits.Add(new MeleeAttackHit(rayHit, hitbox.weaponPosCurrent, hitbox.weaponPosPrev, hitTime));
+                            earliestHitTime = hitTime;
+                        }
+                    }
+                }
+            }
+
+            // There will only be one hit here, unless spear
+            foreach (MeleeAttackHit attackHit in attackHits)
+            {
+                IDamageable damagable = attackHit.rayHit.collider.GetComponent<IDamageable>();
+
+                // Remove hits that occurred after we hit an obstacle when using SphereCastAll
+                if (attackHit.hitTime > earliestObstacleHitTime)
+                    continue;
+
+                // Check if we've already hit this collider ( this is the first check, so no )
+                // and also sets identifier so we can make sure we don't double-tap it below
+                if (ConsiderDamageable(damagable))
+                {
+#if DEBUG_GTFO_VR
+                    DebugDraw3D.DrawCone(attackHit.weaponPosCurrent, attackHit.weaponPosPrev, m_hitboxSize * 0.3f, ColorExt.Blue(0.5f), DEBUG_HIT_DRAW_DURATION);
+#endif
+
+                    hits.Add(new MeleeWeaponDamageData
+                    {
+                        damageGO = attackHit.rayHit.collider.gameObject,
+                        hitPos = attackHit.rayHit.point, // vector from sourcePos to hitPos, and source to enemy spine used to determine backstab
+                        hitNormal = attackHit.rayHit.normal, // Used for gore
+                        sourcePos = attackHit.rayHit.point - (attackHit.Velocity.normalized), // positioned used to calculate backstab
+                        damageTargetFound = true, // Not actually used for anything
+                        damageComp = damagable
+                    });
                 }
             }
 
             // If there are no hits, it is possible that we are already inside of a collider. 
-            // Also do this if we are allowed to hit multiple enemies ( e.g. spear )
-            if (!castHit || m_weapon.MeleeArchetypeData.CanHitMultipleEnemies)
+            // Also do this if we are allowed to hit multiple enemies ( e.g. spear ), but not if we already impact some obstacle
+            if (hits.Count < 1 || earliestObstacleHitTime == float.MaxValue && m_weapon.MeleeArchetypeData.CanHitMultipleEnemies ) 
             {
                 foreach (var hitbox in hitboxes)
                 {
-                    var (weaponPosCurrent, weaponPosPrev) = (hitbox.weaponPosCurrent, hitbox.weaponPosPrev);
-                    Vector3 velocity = (weaponPosCurrent - weaponPosPrev);
-
-                    var colliders = Physics.OverlapSphere(weaponPosCurrent, m_hitboxSize, LayerManager.MASK_MELEE_ATTACK_TARGETS, QueryTriggerInteraction.Ignore);
+                    var colliders = Physics.OverlapSphere(hitbox.weaponPosCurrent, m_hitboxSize, LayerManager.MASK_MELEE_ATTACK_TARGETS, QueryTriggerInteraction.Ignore);
                     foreach (var collider in colliders)
                     {
                         IDamageable damagable = collider.GetComponent<IDamageable>();
@@ -349,9 +410,9 @@ namespace GTFO_VR.Core.PlayerBehaviours
                             hits.Add(new MeleeWeaponDamageData
                             {
                                 damageGO = collider.gameObject,
-                                hitPos = weaponPosCurrent, // vector from sourcePos to hitPos, and source to enemy spine used to determine backstab
-                                hitNormal = velocity.normalized * -1f, // / Used for gore. Expected to be surface normal of thing we hit
-                                sourcePos = weaponPosCurrent - (velocity.normalized), // positioned used to calculate backstab
+                                hitPos = hitbox.weaponPosCurrent, // vector from sourcePos to hitPos, and source to enemy spine used to determine backstab
+                                hitNormal = hitbox.Velocity.normalized * -1f, // / Used for gore. Expected to be surface normal of thing we hit
+                                sourcePos = hitbox.weaponPosCurrent - (hitbox.Velocity.normalized), // positioned used to calculate backstab
                                 damageTargetFound = true, // Not actually used for anything
                                 damageComp = damagable
                             });
